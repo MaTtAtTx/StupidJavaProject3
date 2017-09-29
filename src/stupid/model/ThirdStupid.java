aa609af5c0db7954aa1f8d374cd12b12980c13ab
mattitude17 <mjleo4056@gmail.com>
2017-09-29 15:07:58 -0600

    Added a display class and model class to test different functions.

--- src/stupid/model/ThirdStupid.java
+++ src/stupid/model/ThirdStupid.java
@@ -2,5 +2,41 @@ package stupid.model;
 
 public class ThirdStupid
 {
+	private String name;
+	private int age;
 
+	public ThirdStupid(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	// Getters
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	// Setters
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public string toSting()
+	{
+		String description = "This is testing name as " + name + "and age as " + age + ".";
+
+		return description;
+	}
 }
--- src/stupid/vew/StupidDisplay.java
+++ src/stupid/vew/StupidDisplay.java
@@ -1,6 +1,22 @@
 package stupid.vew;
 
+import javax.swing.JOptionPane;
+
 public class StupidDisplay
 {
+	//Allows you to have popups that display text
+	public void displayText(String textToDisplay)
+	{
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+	
+	//Allows you to get a response in a popup display
+	public String getResponse(String questionAsked)
+	{
+		String response = "";
 
+		response += JOptionPane.showInputDialog(null, questionAsked);
+
+		return response;
+	}
 }
